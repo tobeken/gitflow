3d125a1e511fb53fbe6a1b479dd5437e6a197198
tobeken <tobik0124@gmail.com>
2024-02-29 10:09:24 +0900

    todoapp layout

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-32 py-4 px-4">
+      <div className="px-4 py-2">
+        <h1 className="text-gray-800 font-bold text-2xl uppercase">
+          To-Do List
+        </h1>
+      </div>
+      <form
+        className="w-full max-w-sm mx-auto px-4 py-2"
+     //   onSubmit={(e: React.FormEvent) => addTodo(e)}
+      >
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            // onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            //   setTitle(e.target.value)
+            // }
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Add a task"
+  //          value={title}
+          />
+          <button
+            className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+      <ul className="divide-y divide-gray-200 px-4">
+        {/* {data?.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))} */}
+      </ul>
+    </div>
+
+    </>
+  )
+}
+
+export default App
